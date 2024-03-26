c777588fdd22cc0fe32b6154ec40ec9a96660dde
KwibooReese <reesehayman@gmail.com>
2024-03-26 12:41:28 +0000

    Added placeholder name and image

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,12 @@
+import PostingUser from "./PostingUser";
+
 const Post = (props) => {
-  return <article key={props.post._id}>{props.post.message}</article>;
+  return (
+    <>
+      <article key={props.post._id}>{props.post.message}</article>
+      <PostingUser post={props.post} />
+    </>
+  );
 };
 
 export default Post;
---
+++ frontend/src/components/Post/PostingUser.jsx
@@ -0,0 +1,10 @@
+const PostingUser = ({post}) => {
+  console.log(post);
+  return (<div className="postUserInfo">
+  <div>Reese Hayman</div>
+  <img className="profilePicture"src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"></img>
+  </div>
+  );
+};
+
+export default PostingUser;
