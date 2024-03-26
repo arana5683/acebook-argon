a09b7b595abcaa987c7977fc97c6e02cdf08666a
Reese Hayman <108630606+KwibooReese@users.noreply.github.com>
2024-03-26 16:05:59 +0000

    Update PostingUser.jsx

--- frontend/src/components/Post/PostingUser.jsx
+++ frontend/src/components/Post/PostingUser.jsx
@@ -1,7 +1,6 @@
 import "./PostingUser.css"
 
 const PostingUser = ({post}) => {
-  console.log(post);
   return (<div className="PostingUser">
   <div className="name">Reese Hayman</div>
   <img className="profilePicture"src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"></img>
