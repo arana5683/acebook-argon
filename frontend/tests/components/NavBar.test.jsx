44f1257b9fcd0f235e41211afb61716d61711dbf
Jess <jessicacktodd@gmail.com>
2024-03-27 09:50:59 +0000

    wrote some tests and finished NavBar

---
+++ frontend/tests/components/NavBar.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { FeedPage } from '../../src/pages/Feed/FeedPage';
+import { vi } from "vitest";
+import { NavBar } from '../../src/components/NavBar';
+
+vi.mock("react-router-dom", () => {
+    const navigateMock = vi.fn();
+    const useNavigateMock = () => navigateMock; // Create a mock function for useNavigate
+    return { useNavigate: useNavigateMock };
+});
+
+describe("NavBar", () => {
+    test('page should have a button', () => {
+        render(<NavBar />);
+        expect(screen.getByText("Log Out"));
+});
+
+    test('page should have a button', () => {
+        render(<NavBar />);
+        expect(screen.getByText("My Profile"));
+});
+
+    test('page should have a button', () => {
+        render(<NavBar />);
+        expect(screen.getByText("Homepage"));
+});
+})
