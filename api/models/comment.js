537e0371535e3f8ddd7f7785bbb0e0ddad0d5d04
George Paul <george.paul5236@gmail.com>
2024-04-02 09:49:26 +0100

    committing before switch

--- api/models/comment.js
+++ api/models/comment.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema({
 body: String,
-userName: String,
+firstName: String,
+lastName: String,
 userID: String,
 parentID: String,
 });
@@ -10,7 +11,8 @@ parentID: String,
 const Comment = mongoose.model("Comment", CommentSchema);
 
 new Comment({body: "First Comment", 
-userName: "AwesomeUser", 
+firstName: "Guy",
+lastName: "incognito",
 userID: "ID", 
 parentID: "660155fa0a81a51974be11c8"}).save();
 
