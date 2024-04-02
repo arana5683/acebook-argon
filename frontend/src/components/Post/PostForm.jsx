e01a915cc6657e1af4378ebed2de931233bd6f61
Jess <jessicacktodd@gmail.com>
2024-04-02 15:21:44 +0100

    added frontend changes

--- frontend/src/components/Post/PostForm.jsx
+++ frontend/src/components/Post/PostForm.jsx
@@ -3,19 +3,32 @@ import { useState } from "react";
 
 const PostForm = (props) => {
     const [text, setText] = useState('');
+    const [file, setFile] = useState(null);
+    // initialises file and sets it as null to indicate no file selected 
 
     const handleChange = (event) => {
         setText(event.target.value);
     };
 
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+    // this is triggered when user selects a file - updates file state - event.target.files 
+    // represents files selected - [0] is first file.
+
     const handlePost = () => {
-        props.handleNewPost(text);
+
+        
+        props.handleNewPost(text, file);
         setText('');
-    }
+        setFile(null);
+    };
+    // resets the state to its value
 
     return(
         <>
             <textarea name="text-area" placeholder="What's on your mind?" value={text} onChange={handleChange}></textarea>
+            <input type="file" accept="image/png, image/jpeg" onChange={handleFileChange} />
             <button role = "post-button" onClick={handlePost}>Post!</button>
         </>
     )
