df31547b797713b114e9d6b4213f8daa67d0ee6b
Aakash Rana <aakashrana.r4z@gmail.com>
2024-04-03 12:33:53 +0100

    Added route and controller for posting comments + tests

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const CommentsController = require("../controllers/comments");
 
 router.get("/", CommentsController.getAllComments);
+router.post("/", CommentsController.postNewComment);
 
 module.exports = router;
