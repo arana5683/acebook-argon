3ddec6ef437d3fe2c383986bcdc61327d73c05d3
Aakash Rana <aakashrana.r4z@gmail.com>
2024-04-03 12:22:31 +0100

    Name changes on model for consistency. Removed test comments

--- api/models/comment.js
+++ api/models/comment.js
@@ -4,24 +4,10 @@ const CommentSchema = new mongoose.Schema({
 body: String,
 firstName: String,
 lastName: String,
-userID: String,
-parentID: String,
+userId: String,
+parentId: String,
 });
 
 const Comment = mongoose.model("Comment", CommentSchema);
-
-// Test comments below - adjust their ParentID to match a postID in your posts database to see them on post.
-
-// new Comment({body: "First Comment", 
-// firstName: "Guy",
-// lastName: "incognito",
-// userID: "ID", 
-// parentID: "660155fa0a81a51974be11c8"}).save();
-
-// new Comment({body: "Nice post!", 
-// firstName: "Larry", 
-// lastName: "Facebook", 
-// parentID: "660159280a81a51974be11e6", 
-// userID: "ID"}).save()
 
 module.exports = Comment;
