9fe5659ed72cb8aef7bfa028d0209051468b7dd1
Aakash Rana <aakashrana.r4z@gmail.com>
2024-04-04 15:56:00 +0100

    Added frontend handling for following/unfollowing user

--- frontend/src/components/Post/PostingUser.jsx
+++ frontend/src/components/Post/PostingUser.jsx
@@ -1,10 +1,46 @@
 import "./PostingUser.css"
+import { followUser, isUserFollowed } from "../../services/users";
+import { useEffect, useState } from "react";
 
 const PostingUser = ({post}) => {
-  return (<div className="PostingUser">
-  <div className="name">{post.firstName} {post.lastName}</div>
-  <img className="profilePicture"src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"></img>
-  </div>
+  const [ following, setFollowing ] = useState(false);
+  const [ self, setSelf ] = useState(false);
+
+  useEffect(() => {
+    try {
+      const token = localStorage.getItem("token");
+      const targetId = post.userId;
+      isUserFollowed(token, targetId)
+      .then((res) => {
+        if (res.self)
+          setSelf(true);
+        else {
+          setFollowing(res.followed);
+          setSelf(false);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, [post.userId]);
+
+  const handleFollow = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const targetId = post.userId;
+      await followUser(token, targetId);
+      setFollowing(!following);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  return (
+    <div className="PostingUser">
+    <div className="name">{post.firstName} {post.lastName}</div>
+    <img className="profilePicture"src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"></img>
+    <div className="followButton">{!self && <button onClick={ handleFollow }>{following ? "Unfollow" : "Follow" }</button>}</div>
+    </div>
   );
 };
 
--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -18,3 +18,63 @@ const requestOptions = {
   return data;
 
 };
+
+export const followUser = async (token, targetId) => {
+  const target = { "targetId": targetId };
+  const requestOptions = {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(target),
+  };
+
+  const response = await fetch(`${BACKEND_URL}/users/follow`, requestOptions);
+  
+  if (response.status !== 200) {
+    console.log(response.status);
+    throw new Error(
+      `Received status ${response.status}. Expected 200. Unable to follow user.`);
+  }
+}
+
+export const unfollowUser = async (token, targetId) => {
+  const target = { "targetId": targetId };
+  const requestOptions = {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(target),
+  };
+
+  const response = await fetch(`${BACKEND_URL}/users/unfollow`, requestOptions);
+  
+  if (response.status !== 200) {
+    console.log(response.status);
+    throw new Error(
+      `Received status ${response.status}. Expected 200. Unable to unfollow user.`);
+  }
+}
+
+export const isUserFollowed = async (token, targetId) => {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await fetch(`${BACKEND_URL}/users/following?targetId=${targetId}`, requestOptions);
+  
+  if (response.status !== 200) {
+    console.log(response.status);
+    throw new Error(
+      `Received status ${response.status}. Expected 200. Unable to check if user is already followed.`);
+  }
+
+  const data = await response.json();
+  return data;
+}
