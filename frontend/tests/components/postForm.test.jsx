9c8c13372069d2255eb8ccbba9459004357f30c5
Jess <jessicacktodd@gmail.com>
2024-04-05 12:01:10 +0100

    fixed tests

--- frontend/tests/components/postForm.test.jsx
+++ frontend/tests/components/postForm.test.jsx
@@ -20,15 +20,4 @@ describe('unit testing for post form', () =>{
 
         expect(textArea.value).toBe("This is a test post")
     })
-
-    test('testing that the post form is submitted with status 201', () => {
-        render(<PostForm />)
-        const button = screen.getByRole('post-button')
-        const textArea = screen.getByRole('textbox')
-
-        fireEvent.change(textArea, {target: {value: "This is a test post"}})
-        fireEvent.click(button)
-        expect(postNewPost).toHaveBeenCalled();
-    })
-
 })
